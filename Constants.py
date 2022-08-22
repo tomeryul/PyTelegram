1609a957bf83c38a4db72cd64ab9e6d0a4a09c41
tomeryul <109019979+tomeryul@users.noreply.github.com>
2022-08-22 21:19:03 +0300

    update gitignore

--- Constants.py
+++
@@ -1,5 +0,0 @@
-API_KEY = '5507920468:AAG8el45NoU-wjxhrFCe9OdlLSepz-lqmqo'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36'
-}
